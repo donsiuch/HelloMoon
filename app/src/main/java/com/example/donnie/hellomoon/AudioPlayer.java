ea68cc6c3a37af17169800040e50efa11254a321
donnie <dsiuchninski@gmail.com>
2015-01-15 21:00:20 -0600

    Initial commit

---
+++ app/src/main/java/com/example/donnie/hellomoon/AudioPlayer.java
@@ -0,0 +1,30 @@
+package com.example.donnie.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by donnie on 1/14/15.
+ */
+public class AudioPlayer {
+    private MediaPlayer mPlayer;
+
+    public void stop(){
+        if (mPlayer != null){
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context c){
+        mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+            public void onCompletion(MediaPlayer mp){
+                stop();
+            }
+        });
+
+        mPlayer.start();
+    }
+}
